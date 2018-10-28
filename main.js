72dad31851b3d842feb0a2628fee3c4758475205
Ryan Soto <rsoto003@ucr.edu>
2018-10-28 16:45:44 -0700

    added dynamic table function

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-let 
+var calCount;
+var carbCount;
+var day;
+var foodName;
+var foodType;
+var fatCount;
 
 $(document).ready(initializeApp);
 
 function initializeApp(){
-    console.log('we are clear for liftoff')
+    $(".submitButton").on('click', submitHandler);
+
+}
+
+function submitHandler(){
+    day = $("#daySelect").val();
+    calCount = $("#calCount").val();
+    foodType = $("#foodType").val();
+    foodName = $("#foodName").val();
+    carbCount = $("#carbCount").val(); 
+    fatCount = $("#fatCount").val();
+    var tableRow = $("<tr>");
+    var dayData = $("<td>").text(day);
 }
