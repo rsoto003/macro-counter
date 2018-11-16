2d1a790b68cc5b26021d70b480bd402a614a5cd5
Ryan Soto <rsoto003@ucr.edu>
2018-11-16 15:56:54 -0800

    added section for total calorie data in table

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var day;
 var foodName;
 var foodType;
 var fatCount;
+var totalCalories;
 
 $(document).ready(initializeApp);
 
@@ -20,21 +21,22 @@ function submitHandler() {
     fatCount = $("#fatCount").val();
     // macro calculations below
 
-    proteinCount = proteinCount * 4 + " cal";   
-    carbCount = carbCount * 4 + " cal";
-    fatCount = fatCount * 9 + " cal";   
+    proteinCount = proteinCount * 4;   
+    carbCount = carbCount * 4;
+    fatCount = fatCount * 9;   
+    totalCalories = proteinCount + carbCount + fatCount + "cal";
+    console.log(totalCalories);
     
     var tableRow = $("<tr>");
     var dayData = $("<th>").text(day);
     var typeData = $("<td>").text(foodType);
     var foodNameData = $("<td>").text(foodName);
-    var proteinCountData = $("<td>").text(proteinCount);
-    var carbCountData = $("<td>").text(carbCount);
-    var fatCountData = $("<td>").text(fatCount);
+    var proteinCountData = $("<td>").text(proteinCount + "cal");
+    var carbCountData = $("<td>").text(carbCount + "cal");
+    var fatCountData = $("<td>").text(fatCount +"cal");
+    var totalCalorieData = $("<td>").text(totalCalories);
 
-    var fatCountData = $("<td>").text(fatCount);
-
-    tableRow.append(dayData, typeData, foodNameData, proteinCountData, carbCountData, fatCountData);
+    tableRow.append(dayData, typeData, foodNameData, proteinCountData, carbCountData, fatCountData, totalCalorieData);
     
     $("tbody").append(tableRow);
     clearInputs();
